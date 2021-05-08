7f7cd24e422c9186e0a3e44a1d29fbf9be122ab8
admin <admin@gmail.com>
2021-05-08 23:52:06 +0600

    test

--- pay.py
+++ pay.py
@@ -1,22 +1,3 @@
-# class Car:
-#     owner = 'Avtandil'
-#     make = 'Ford'
-#     model = 'Raptor'
-#     year = '2021'
-#     odometer = 0
-# ford = Car()
-
-# class Airplane(Car):
-#     make = 'USA Airplanes'
-#     model = 'Boing 400-800'
-#     year = '2018'
-#     # odometer = 0
-
-# boing = Airplane()
-
-# print(boing.odometer)
-
-
 #Инкапсуляция
 class B:
     count = 0
