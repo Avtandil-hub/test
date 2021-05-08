ff86d25e939d65a811903b3b2b28fea301873c6f
admin <admin@gmail.com>
2021-05-09 00:15:54 +0600

    test

--- pay.py
+++ pay.py
@@ -1,12 +1,18 @@
-#Инкапсуляция
-class B:
-    count = 0
-    def __init__(self):
-        B.count += 1
-    def __del__(self):
-        B.count -= 1
-    def _end(self):
-        print('end')
-a = B()
-print(a.count)
-a._end()
+# class Car:
+#     owner = 'Avtandil'
+#     make = 'Ford'
+#     model = 'Raptor'
+#     year = '2021'
+#     odometer = 0
+# ford = Car()
+
+# class Airplane(Car):
+#     make = 'USA Airplanes'
+#     model = 'Boing 400-800'
+#     year = '2018'
+#     # odometer = 0
+
+# boing = Airplane()
+
+# print(boing.odometer)
+
